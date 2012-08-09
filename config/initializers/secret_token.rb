bc6037025c3d117f9b83e7df6dd0b94a209eb632
Maxim Filimonov <tpaktopsp@gmail.com>
2012-08-09 23:31:36 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SalesCatalog::Application.config.secret_token = '56b8a4cc8b0ce12fa4928d1f1f562e4d728fbb1e6f5645b61e125b81351e0f6336806a923268bdd61ddc96f5285d0a13c9f8b3fde33b64acb461cca73b9a3ad8'
