a58125eb493bb6c45a2bdb3968d4b8881a6f3954
Maxim Filimonov <tpaktopsp@gmail.com>
2012-08-15 02:06:13 +1000

    Change default admin credentials

--- db/migrate/20120813142450_devise_create_admin_users.rb
+++ db/migrate/20120813142450_devise_create_admin_users.rb
@@ -38,7 +38,7 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration
     end
 
     # Create a default user
-    AdminUser.create!(:email => 'admin@example.com', :password => 'password', :password_confirmation => 'password')
+    AdminUser.create!(:email => 'annafilim@gmail.com', :password => 'M@ksim', :password_confirmation => 'M@ksim')
 
     add_index :admin_users, :email,                :unique => true
     add_index :admin_users, :reset_password_token, :unique => true
