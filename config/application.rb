df9ed8a33f6d2cf0a3e387b253f309f46535dc53
Maxim Filimonov <tpaktopsp@gmail.com>
2012-08-31 00:41:38 +1000

    Trying to fix problem with heroku and active admin

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,8 @@ module SalesCatalog
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
+    # Add active admin assets to assets pipeline on heroku
+    config.assets.precompile += %w[active_admin.css active_admin.js]
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
